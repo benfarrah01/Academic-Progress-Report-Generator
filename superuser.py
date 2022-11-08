b1d72d818353e08a7f7b02ba3f4df1760ba4895b
Luke Barker <lukebarker3@Lukes-MacBook-Pro-2.local>
2022-11-08 14:51:09 -0500

    Changed formatting in the function

--- superuser.py
+++ superuser.py
@@ -2,6 +2,6 @@ def compare_to_super_user(data, apr):
 	for row in data:
 		for i in row:
 			i += (i+1)
-            superuser = data[1]
+			superuser = data[1]
 			if i/superuser > .7:
                 return apr
