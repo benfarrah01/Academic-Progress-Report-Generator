6462791b032c6c20a5a10868a71712917edd94e0
Luke Barker <lukebarker3@Lukes-MacBook-Pro-2.local>
2022-12-06 14:42:52 -0500

    Added changes to superuser file

--- superuser.py
+++ superuser.py
@@ -1,10 +1,13 @@
 from access_sheets import run
 from APR_Generator import apr_generator
+from Sample_Data_Structure import Students
 
-def compare_to_super_user(data, apr):
-	for row in data:
-		for i in row:
-			i += (i+1)
-			superuser = data[1]
-			if i/superuser > .7:
+def compare_to_super_user():
+    apr = "You need to do better"
+    data = Students
+    for row in data:
+        for i in row:
+            i += (i+1)
+            superuser = data[1]
+            if i/superuser > .7:
                 return apr
