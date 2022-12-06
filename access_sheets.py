4ea702b7f9230759fe777c4998a83d7df9046312
benfarrah01 <farrah01@allegheny.edu>
2022-12-06 16:28:11 -0500

    Format data from sheet as CSV

--- access_sheets.py
+++ access_sheets.py
@@ -1,3 +1,5 @@
+import csv
+
 """"This file, when put into `SheetShuttle/plugins`, can be run to display the contents of the sheet."""
 from sheetshuttle import sheet_collector
 import pandas as pd
@@ -9,24 +11,46 @@ key_file='new_key.json'
 sources_dir='config/sheet_sources'
 
 def run(sheets_keys_file, sheets_config_directory, **kwargs):
-    """Example run function. Collect and display info from google sheet."""
-    # Create SheetCollector object named `my_collector`
-    #   Created with args `key_file` and `sources_dir`
-    my_collector = sheet_collector.SheetCollector(key_file, sources_dir)
-    my_collector.collect_files()
-    # Display contents of the sheet
+     """Example run function. Collect and display info from google sheet."""
+     # Create SheetCollector object named `my_collector`
+     #   Created with args `key_file` and `sources_dir`
+     my_collector = sheet_collector.SheetCollector(key_file, sources_dir)
+     my_collector.collect_files()
+     # Display contents of the sheet
 
-    #my_collector.print_contents()
+     #my_collector.print_contents()
 
-    # print((type(my_collector.sheets_data)))
-    # print(my_collector.sheets_data.values)
+     # print((type(my_collector.sheets_data)))
+     # print(my_collector.sheets_data.values)
 
-    #my_sheet = sheet_collector.Sheet()
-    
-    for key, s in my_collector.sheets_data.items():
-         print(key,s.regions)
-    
-    
-    #my_collector.sheets_data["APR-Generator-Config"].regions["Students_Info"].print_region()
-    for key, value in my_collector.sheets_data["APR-Generator-Config"].regions:
-          print(key,value)
+     #my_sheet = sheet_collector.Sheet()
+
+     #     for key, s in my_collector.sheets_data.items():
+     #          print(key,s.regions)
+
+
+     #     #my_collector.sheets_data["APR-Generator-Config"].regions["Students_Info"].print_region()
+     #     for key, value in my_collector.sheets_data["APR-Generator-Config"].regions:
+     #           print(key,value) 
+
+     # for region_id, region in my_collector.sheets_data["APR-Generator-Config"].regions["Sheet1_Students_Info"]:
+     #        print(f"******\t {region_id} \t ******")
+     #        region.print_region()
+     #        print("*********************************")
+
+
+     my_data = my_collector.sheets_data["APR-Generator-Config"].regions["Sheet1_Students_Info"].data  
+     print(my_data)
+     print(type(my_data))
+     print('\n\n\n')
+
+     csv_data = my_data.to_csv()
+     print(csv_data)
+     print(type(csv_data))
+
+     # for region in my_collector.sheets_data["APR-Generator-Config"].regions:
+     #      print(region)
+
+     #for fuckyou in my_collector.sheets_data["APR-Generator-Config"].regions:
+     #my_collector.print_contents()
+     
