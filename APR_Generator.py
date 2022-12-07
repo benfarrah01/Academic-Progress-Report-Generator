f702054fa922d995e207973c7eab8eac6e3a4360
Bergas Anargya <bergasanargya@Bergass-MacBook-Air.local>
2022-12-06 19:02:21 -0500

    Some edit on APR_Generator

--- APR_Generator.py
+++ APR_Generator.py
@@ -2,8 +2,11 @@ from Sample_Data_Structure import Students
 
 from re import X
 
+from typing import List
+# from typing import Dict
 
-All_assignments = {'Assignments 1': 45, 'Assignment 2' : 50, 'Assignment 3' : 75, 'Assignment 4' : 90, 'Assignment 5' : 55} 
+
+# All_assignments = {'Assignments 1': 45, 'Assignment 2' : 50, 'Assignment 3' : 75, 'Assignment 4' : 90, 'Assignment 5' : 55} 
 Assignments = []
 #print Assignments()
 
@@ -12,25 +15,30 @@ def assignment_filter(All_assignments:dict):
     for key in All_assignments.keys():
         Assignment = All_assignments.get(key,X)
         if Assignment <= 60:
-            Assignments.append(key) 
+            Assignments.append(key)
 
 
-def display_assignment(Assignments:list): 
-    """Print each Assignment idividually"""
+def display_assignment(Assignments:List[int]): 
+    """Print each Assignment individually"""
     for Assignment in Assignments: 
         print (f"* {Assignment}")
 
     
 
-def apr_generator(): 
+def apr_generator(current_grade: int, student_name: str, assigned_class: str, days_missed: int): 
     """This function will access List of APR'S and create them"""
-    current_grade = 60 
-    Student_name = "Luke"  
-    assigned_class = "CMPSC 300" 
-    days_missed = 12
-    #Assignments = {'Assignments_1': 45, 'Assignment_2' : 50, 'Assignment_3' : 75, 'Assignment_4' : 90, 'Assignment_5' : 55} 
-    print(f"Hello {Student_name}, This is a notice that you are receiving an academic alert in {assigned_class}. You currently have a {current_grade} in this class as you are either missing or received less than a 60% on these assignments: ")
+    # current_grade = 60 
+    # Student_name = "Luke"  
+    # assigned_class = "CMPSC 300" 
+    # days_missed = 12
+    # Assignments = {'Assignments_1': 45, 'Assignment_2' : 50, 'Assignment_3' : 75, 'Assignment_4' : 90, 'Assignment_5' : 55} 
+    print(f"Hello {student_name}, This is a notice that you are receiving an academic alert in {assigned_class}. You currently have a {current_grade} in this class as you are either missing or received less than a 60% on these assignments: ")
     display_assignment(Assignments)
     print(f"you have missed {days_missed} days. If this continues you may be in danger of failing this course, please reach out to me to see how you can bring this up, and get back on track for success!")
-assignment_filter(All_assignments)
-apr_generator() 
+
+if __name__ == '__main__':
+    assignment_filter(Students['Student_2']['Assignments'])
+    apr_generator(Students['Student_2']['Current Grade'], Students['Student_2']['Name'], 
+    Students['Student_2']['Assigned Class'], Students['Student_2']['Days Missed'])
+    # assignment_filter(All_assignments)
+    # apr_generator() 
--- Sample_Data_Structure.py
+++ Sample_Data_Structure.py
@@ -1,20 +1,68 @@
-Students = {"Student_1": {"Email": "student1@gmail.com", 
-                         "Advisor": "advisor1", 
-                         "Advisor_Email": "advisor1@gmail.com", 
-                         "total": 0}, 
-            "Student_2": {"Email": "student2@gmail.com", 
-                         "Advisor": "advisor2", 
-                         "Advisor_Email": "advisor2@gmail.com", 
-                         "total": 0},
-            "Student_3": {"Email": "student3@gmail.com", 
-                         "Advisor": "advisor3", 
-                         "Advisor_Email": "advisor3@gmail.com", 
-                         "total": 0},
-            "Student_4": {"Email" : "student4@gmail.com", 
-                         "Advisor" : "advisor4", 
-                         "Advisor_Email" : "advisor4@gmail.com", 
-                         "total" : 0}
+Students = {'Student_1': {'Current Grade': 60, 
+                         'Name': 'Luke', 
+                         'Student Email': 'student1@gmail.com', 
+                         'Assignments': {'Assignment 1': 45, 'Assignment 2' : 50, 'Assignment 3' : 75, 'Assignment 4' : 90, 'Assignment 5' : 55}, 
+                         'Assigned Class': 'CMPSC 300',
+                         'Days Missed': '12',
+                         'Advisor': 'advisor1', 
+                         'Advisor Email': 'advisor1@gmail.com'}, 
+            'Student_2': {'Current Grade': 67, 
+                         'Name': 'Ben', 
+                         'Student Email': 'student1@gmail.com', 
+                         'Assignments': {'Assignment 1': 60, 'Assignment 2' : 50, 'Assignment 3' : 85, 'Assignment 4' : 80, 'Assignment 5' : 60}, 
+                         'Assigned Class': 'CMPSC 300',
+                         'Days Missed': '9',
+                         'Advisor': 'advisor1', 
+                         'Advisor Email': 'advisor1@gmail.com'},
+            'Student_3': {'Current Grade': 70, 
+                         'Name': 'Darius', 
+                         'Student Email': 'student1@gmail.com', 
+                         'Assignments': {'Assignment 1': 70, 'Assignment 2' : 70, 'Assignment 3' : 75, 'Assignment 4' : 90, 'Assignment 5' : 45}, 
+                         'Assigned Class': 'CMPSC 300',
+                         'Days Missed': '7',
+                         'Advisor': 'advisor1', 
+                         'Advisor Email': 'advisor1@gmail.com'},
+            'Student_4': {'Current Grade': 73, 
+                         'Name': 'Bergas', 
+                         'Student Email': 'student1@gmail.com', 
+                         'Assignments': {'Assignment 1': 65, 'Assignment 2' : 70, 'Assignment 3' : 75, 'Assignment 4' : 80, 'Assignment 5' : 75}, 
+                         'Assigned Class': 'CMPSC 300',
+                         'Days Missed': '8',
+                         'Advisor': 'advisor1', 
+                         'Advisor Email': 'advisor1@gmail.com'},
+            'Student_5': {'Current Grade': 74, 
+                         'Name': 'Makell', 
+                         'Student Email': 'student1@gmail.com', 
+                         'Assignments': {'Assignment 1': 85, 'Assignment 2' : 50, 'Assignment 3' : 85, 'Assignment 4' : 90, 'Assignment 5' : 65}, 
+                         'Assigned Class': 'CMPSC 300',
+                         'Days Missed': '13',
+                         'Advisor': 'advisor1', 
+                         'Advisor Email': 'advisor1@gmail.com'}
            }
+
+
+print(Students['Student_1']['Assignments']['Assignment 2'])
+
+# print(Students['Student_1']['Current Grade'])
+
+# my_array = []
+
+# for innerdict in Students.values():
+#     for value in innerdict.values():
+#         my_array.append(innerdict)
+
+# grade_array = []
+
+# for i in my_array:      
+#     if my_array.index() % 6 == 0:
+#         grade_array.append[i]
+
+# print(grade_array)
+
+
+# print(my_array)
+
+
 
 
 
