803e483f939535b1b96b1ec94c7aee22261f8e91
Luke Barker <lukebarker3@Lukes-MacBook-Pro-2.local>
2022-12-08 12:09:04 -0500

    Changed superuser.py

--- superuser.py
+++ superuser.py
@@ -3,17 +3,36 @@ from Sample_Data_Structure import Students
 import sys
 
 data = Students
-print(data)
-superuser = data['Student_1']['total']
 
-print(superuser)
-print(data['Student_1'].keys())
-
+i = 0
+k = 0
 for p_id, p_info in data.items():
-    print("\nPerson ID:", p_id)
+    if p_id == "Superuser":
+        for key in p_info:
+            if key == "Total":
+                k = p_info.get(key,0)
+                print(k)
+    else:
 
-    for key in p_info:
-        print(f"{key}: " + f"{p_info[key]}")
+    #print("\nPerson ID:", p_id)
+
+
+        for key in p_info:
+        #print(f"{key}: " + f"{p_info[key]}")
+            if key == "Total":
+                total_value = p_info.get(key,0)
+                if total_value/k < .7:
+                    apr = "You are dumb"
+                    print(apr)
+                #print(total_value)
+
+
+
+
+        #i += 1
+        #if i >= 1:
+            #break
+
     # for key[3] in data:
     #     print(key[3])
 
