162af7a1c1f032c2f6ae02d00af8ff9c8ceddf30
Bergas Anargya <bergasanargya@Bergass-MacBook-Air.local>
2022-12-12 22:47:03 -0500

    Final implementation of the APR is ready.

--- APR_Generator.py
+++ APR_Generator.py
@@ -1,17 +1,13 @@
-from sheetshuttle import sheet_collector
-import pandas as pd
-
 from Sample_Data_Structure import Students
 
 from gmail_api.send_emails import send_message
 from gmail_api.send_emails import gmail_authenticate
 
-from re import X
+# from access_sheets import master_dict
 
-# File containing authentication information. DON'T PUSH `new_key.json` TO GITHUB!!!
-key_file='new_key.json'
-# Directory containing config information for our google sheet
-sources_dir='config/sheet_sources'
+import superuser
+
+from re import X
 
 def assignment_filter(All_assignments:dict):
     """Filter through posted assignments and remove any over 60%"""
@@ -22,79 +18,37 @@ def assignment_filter(All_assignments:dict):
 
 def apr_generator(current_grade: int, student_name: str, assigned_class: str, days_missed: int): 
     """This function will access List of APR'S and create them"""
-    with open('apr.txt', 'w') as f:
+    with open((str(student_name) + '_apr.txt'), 'w') as f:
         print(f"Hello {student_name}, This is a notice that you are receiving an academic alert in {assigned_class}. ", file = f)
         print(f"You currently have a {current_grade} in this class as you are either missing or received less than a 60% on these assignments:", file = f)
         global Assignments
         for Assignment in Assignments: 
             print (f"* {Assignment}", file = f)
         print(f"you have missed {days_missed} days. If this continues you may be in danger of failing this course, please reach out to me to see how you can bring this up, and get back on track for success!", file = f)
-
-def run(sheets_keys_file, sheets_config_directory, **kwargs):
-     global master_dict
-     """Example run function. Collect and display info from google sheet."""
-     # Create SheetCollector object named `my_collector`
-     #   Created with args `key_file` and `sources_dir`
-     my_collector = sheet_collector.SheetCollector(key_file, sources_dir)
-     my_collector.collect_files()
-
-     # collect data from each region of each sheet
-     my_data = my_collector.sheets_data["APR-Generator-Config"].regions["Sheet1_Students_Info"].data  
-
-     # convert data to a dictionary
-     dict_data = my_data.to_dict()
-     
-     # create new dictionary of dictionaries with a specified structure
-     for count in range(len(dict_data["Name"])):
-          # create initial dict
-          if count == 0:
-               master_dict = make_dict(
-                    dict_data["Name"][count],
-                    dict_data["Email"][count],
-                    dict_data["Advisor"][count],
-                    dict_data["Advisor Email"][count],
-                    dict_data["Total"][count],
-               )
-          # add to initial dict
-          else:
-               my_dict = add_info_to_dict(
-                    master_dict,
-                    dict_data["Name"][count],
-                    dict_data["Email"][count],
-                    dict_data["Advisor"][count],
-                    dict_data["Advisor Email"][count],
-                    dict_data["Total"][count],
-               ) 
-def make_dict(studentname, studentemail, advisorname, advisoremail, points):
-     my_dict = {
-          studentname: {
-               "Email": studentemail,
-               "Advisor": advisorname, 
-               "Advisor_Email": advisoremail, 
-               "total": points}, 
-     }
-     return my_dict
-
-# update nested dict with new values (turn new values into dict before updating nested dict)
-def add_info_to_dict(master_dict, studentname, studentemail, advisorname, advisoremail, points):
-     my_dict = make_dict(studentname, studentemail, advisorname, advisoremail, points)
-     master_dict.update(my_dict)
-
-# updated nested dict with new dict
-def add_dict_to_dict(master_dict, my_dict):
-     master_dict.update(my_dict)
+        
 
 if __name__ == '__main__':
     Assignments = []
-    name_input = input("Who do you want to send the APR to? ")
-    for i in range(len(Students)):
-        if name_input == (Students['Student_' + str(i+1)]['Name']):
+    for i in range(len(Students) - 1):
+        if superuser.compare_stu_super(Students['Student_' + str(i + 1)]['Total']) == True:
             assignment_filter(Students['Student_' + str(i+1)]['Assignments'])
-            apr_generator(Students['Student_' + str(i+1)]['Current Grade'], Students['Student_' + str(i+1)]['Name'], 
+            apr_generator(Students['Student_' + str(i+1)]['Total'], Students['Student_' + str(i+1)]['Name'], 
             Students['Student_' + str(i+1)]['Assigned Class'], Students['Student_' + str(i+1)]['Days Missed'])
             service = gmail_authenticate()
-            send_message(service, Students['Student_' + str(i+1)]['Student Email'], "This is a subject", 
-    "This is the body of the email", ["apr.txt"])
-        # elif name_input != (Students['Student_' + str(i+1)]['Name']):
-        #     print(f"Name is not in the list. Please try again!")
+            email_subject = "Notice on missing " + str(Students['Student_' + str(i+1)]['Assigned Class']) + " for" + str(Students['Student_' + str(i+1)]['Name'])
+            email_intro = "Dear " + str(Students['Student_' + str(i+1)]['Name']) + ", " + "\n" + "\n"
+            email_body = email_intro + "You have received this APR due to academic performance in " + str(Students['Student_' + str(i+1)]['Assigned Class']) + ". Please check this APR to on what you are missing in this class. "
+            send_message(service, Students['Student_' + str(i+1)]['Student Email'], email_subject, 
+    email_body, [str(Students['Student_' + str(i+1)]['Name']) + '_apr.txt'])
+
+
+
+    # my_dict = master_dict()
+    # for i in range(len(my_dict)):
+    #     if name_input in (my_dict.keys()):
+    #         assignment_filter(my_dict.keys()[i])
+    #     elif name_input != (Students['Student_' + str(i+1)]['Name']):
+    #         print(f"Name is not in the list. Please try again!")
+
+# Every time a student has a total percentage less than 70% of superuser, then send the APR
             
--- superuser.py
+++ superuser.py
@@ -4,37 +4,37 @@ import sys
 
 data = Students
 
-i = 0
-k = 0
-for p_id, p_info in data.items():
-    if p_id == "Superuser":
-        for key in p_info:
-            if key == "Total":
-                k = p_info.get(key,0)
-                print(k)
+def compare_stu_super(total_value):
+    k = 0
+    for p_id, p_info in data.items():
+        if p_id == "Superuser":
+            for key in p_info:
+                if key == "Total":
+                    k = p_info.get(key,0)
+                    # print(k)
+        # else:
+        #     for key in p_info:
+        #         if key == "Total":
+        #             # total_value = p_info.get(key,0)
+        #             if total_value/k < .7:
+        #                 apr = "You are dumb"
+        #                 print(apr)
+    if total_value/k < .7: 
+        return True
     else:
-
-    #print("\nPerson ID:", p_id)
-
-
-        for key in p_info:
-        #print(f"{key}: " + f"{p_info[key]}")
-            if key == "Total":
-                total_value = p_info.get(key,0)
-                if total_value/k < .7:
-                    apr = "You are dumb"
-                    print(apr)
-                #print(total_value)
-<<<<<<< HEAD
-=======
-
-def compare_stu_super(super_user, user):
-    score = user / super_user
-    if score <= .7:
-        return True 
-    elif score >= .7:
         return False
-    return score
+
+    
+                #print(total_value)
+
+# def compare_stu_super(super_user, user):
+#     # user = input("User score: ")
+#     # super_user = input("Superuser score: ")
+#     score = user / super_user
+#     if score <= .7:
+#         return True 
+#     else:
+#         return False
 
 
 
@@ -57,4 +57,3 @@ def compare_stu_super(super_user, user):
 #             superuser = data[4]
 #             if i/superuser > .7:
 #                 return apr
->>>>>>> 4ab82a674b311bb1c63112d4747d427cd78292b1
