d86c25e29664ffd5b1838694cd03bb18c0e123fe
Makell-Logan <logan01@allegheny.edu>
2022-12-16 05:24:34 -0500

    Update: Updated test_APR_Generator.py

--- tests/test_APR_Generator.py
+++ tests/test_APR_Generator.py
@@ -1,7 +1,13 @@
 import pytest
 import APR_Generator
+from Sample_Data_Structure import Students
 
 @pytest.mark.parametrize("All_Assignments, current_grade,student_name,assigned_class,days_missed", {})
 def test_assignment_filter(All_Assignments):
+    output = APR_Generator.assignment_filter(Students)
+    expected_output = True
+    if output == type(list):
+        output = True
+    assert expected_output == output
 
-def test_apr_generator(current_grade: int, student_name: str, assigned_class: str, days_missed: int):
+# def test_apr_generator(current_grade: int, student_name: str, assigned_class: str, days_missed: int):
