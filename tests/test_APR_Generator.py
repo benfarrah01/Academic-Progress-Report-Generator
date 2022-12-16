df58355024b523e8f523255f44793ca9cd28e44e
Makell-Logan <logan01@allegheny.edu>
2022-12-16 17:36:45 -0500

    Update: Updated test_assignment_filter

--- tests/test_APR_Generator.py
+++ tests/test_APR_Generator.py
@@ -2,12 +2,15 @@ import pytest
 import APR_Generator
 from Sample_Data_Structure import Students
 
-@pytest.mark.parametrize("All_Assignments, current_grade,student_name,assigned_class,days_missed", {})
-def test_assignment_filter(All_Assignments):
-    output = APR_Generator.assignment_filter(Students)
-    expected_output = True
-    if output == type(list):
-        output = True
-    assert expected_output == output
+students = Students
 
-# def test_apr_generator(current_grade: int, student_name: str, assigned_class: str, days_missed: int):
+@pytest.mark.parametrize("All_Assignments", students )
+def test_assignment_filter(All_Assignments):
+    passed_assignments = 0
+    expected_passed_assignments = 0
+    failed_assignments = APR_Generator.assignment_filter(All_Assignments)
+    for assignment in failed_assignments:
+        if assignment > 60:
+            passed_assignments += 1
+            assert expected_passed_assignments != passed_assignments
+    assert expected_passed_assignments == passed_assignments
