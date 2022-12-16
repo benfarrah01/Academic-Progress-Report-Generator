ef804281983e51f2c59a2b2facd65be8dcc68995
Makell-Logan <logan01@allegheny.edu>
2022-12-16 04:19:33 -0500

    Added: Test file for APR_Generator.py

---
+++ tests/test_APR_Generator.py
@@ -0,0 +1,7 @@
+import pytest
+import APR_Generator
+
+@pytest.mark.parametrize("All_Assignments, current_grade,student_name,assigned_class,days_missed", {})
+def test_assignment_filter(All_Assignments):
+
+def test_apr_generator(current_grade: int, student_name: str, assigned_class: str, days_missed: int):
