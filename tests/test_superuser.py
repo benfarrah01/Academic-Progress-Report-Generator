614a216f9fd0a57b7161b7c0e13d31e5405b6e5a
Luke Barker <lukebarker3@Lukes-MacBook-Pro-2.local>
2022-12-15 20:07:48 -0500

    Added tests file and started superuser test case

--- tests/test_superuser.py
+++ tests/test_superuser.py
@@ -1,3 +1,7 @@
 import pytest
 import superuser
 
+@pytest.mark.parametrize("total_value, k", [])
+def test_superuser_compared_to_student(total_value, k):
+
+    superuser_score = superuser.compare_stu_super(total_value)
